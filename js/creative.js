a2dfa70b67e7052c9503e40cf2745adb973a1be5
Earl Misquitta <emisquitta@postmedia.com>
2018-11-06 15:15:05 -0500

    Nav, scrolling, updates from sheet, cleanup

--- js/creative.js
+++ js/creative.js
@@ -4,27 +4,13 @@
     var $body = $('body'),
         $window = $(window),
         $navLink = $('.nav-link'),
-        $navLinkActive = ('nav-link-active'),
+        // $navLinkActive = ('nav-link-active'),
         $WO__navbar = $('.navbar'),
         $WO__navbarNav = $('.navbar-nav'),
         $WO__navbarHeight = $WO__navbar.outerHeight(),
         $WO__main = $('#WO__main'),
         $WO__navMobileButtonOpen = $('.WO__nav--mobileButtonOpen'),
         $WO__navMobileButtonClose = $('.WO__nav--mobileButtonClose');
-
-    // Collapse Navbar
-    /* var navbarCollapse = function() {
-        if ($(".navbar").offset().top > 20) {
-            $(".navbar").addClass("navbar-shrink");
-        } else {
-            $(".navbar").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-    // end Collapse Navbar */
 
     // Navigation resize
     $WO__main.css('padding-top', $WO__navbarHeight);
@@ -35,74 +21,6 @@
         $WO__main.css('padding-top', $WO__navbarHeight);
     });
 
-    // Smooth scrolling
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                var $WO__navbarHeight = $WO__navbar.outerHeight();
-
-                $('html, body').animate({
-                    scrollTop: (target.offset().top - $WO__navbarHeight)
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
-    });
-
-    // $('a[href*="#"]:not([href="#"])').click(function(){
-    //     if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'') && location.hostname==this.hostname){
-    //         var target = $(this.hash),
-    //             $WO__rslidesNavHeight = $('.WO__rslidesNav').height();
-
-    //         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-    //         if(target.length){
-    //             if($(this).hasClass('events')){
-    //                 $('html,body').animate({
-    //                     scrollTop: target.offset().top - $WO__rslidesNavHeight
-    //                 },1000);
-    //             }else{
-    //                 $('html,body').animate({
-    //                     scrollTop: target.offset().top
-    //                 },1000);
-    //             }
-                
-    //             return false;
-    //         }
-    //     }
-    // });
-    // end Smooth scrolling
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    /* $('.js-scroll-trigger').click(function() {
-        $('.navbar-collapse').collapse('hide');
-    }); */
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    /*$('body').scrollspy({
-      target: '.navbar',
-      offset: 77
-    });*/
-
-    // Scroll reveal calls
-    window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
-    sr.reveal('.how-works-img img', {
-        duration: 1000,
-        delay: 200
-    });
-    sr.reveal('.goal-image', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 300);
-
     // Intro video
     var $body = $('body'),
         $introImgCon = $('.introImgCon'),
@@ -110,30 +28,53 @@
 
     $headPlayButtonCon.click(function(e){
         var media = $(this).attr('data-media');
-
         $introImgCon.html(media);
-
         $(this).css('display','none');
         e.preventDefault();
     });
 
     // Navigation
+    // Add active class to nav items
     $navLink.click(function(e) {
-        var $this = $(this);
-        $navLink.removeClass($navLinkActive);
-        $this.addClass($navLinkActive);
+    //     var $this = $(this);
+    //     $navLink.removeClass($navLinkActive);
+    //     $this.addClass($navLinkActive);
+        $WO__navMobileButtonClose.trigger('click');
         e.preventDefault();
     });
+
+    // Add active class to nav items on scroll
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: $WO__navbarHeight
+    });
+    // end Add active class to nav items on scroll
+
+    // Add active class to logo on scroll
+    var navbarCollapse = function() {
+        if ($(".navbar").offset().top > 20) {
+            $(".navbar").addClass("navbar-shrink");
+        } else {
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+    // end active class to logo on scroll
+    // end Add active class to nav items
 
     // Nav mobile
     $WO__navMobileButtonOpen.click(function(e){
         $WO__navbarNav.toggleClass('navbar-nav--show');
-
         e.preventDefault();
     });
 
     $WO__navMobileButtonClose.click(function(e){
-        $WO__navbarNav.toggleClass('navbar-nav--show');
+        if ($WO__navbarNav.hasClass('navbar-nav--show')){
+            $WO__navbarNav.toggleClass('navbar-nav--show');
+        }
 
         e.preventDefault();
     });
@@ -145,7 +86,46 @@
     $(document).keyup(function(e){
         if(e.keyCode==27){$WO__navMobileButtonClose.trigger('click');}
     });
+    // end Nav mobile
     // end Navigation
+
+    // Smooth scrolling
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(e) {
+        if (this.hash !== "") {
+            var hash = this.hash,
+                $WO__navbarHeight = $WO__navbar.outerHeight();
+            console.log(hash);
+
+            $('html, body').animate({
+                scrollTop: ($(hash).offset().top)
+            }, {
+                duration: 1000,
+                easing: 'easeInOutExpo',
+                start: function(){
+                    $(hash).css('padding-top', $WO__navbarHeight);
+                }
+            });
+
+            // $('html, body').animate({
+            //     scrollTop: ($(hash).offset().top)
+            // }, {
+            //     start: function(){
+            //         $(hash).css('padding-top', $WO__navbarHeight);
+            //     }
+            // }, 1000, 'easeInOutExpo', function(){});
+                
+            // $('html, body').animate({
+            //     scrollTop: ($(hash).offset().top - $WO__navbarHeight)
+            // }, 1000, "easeInOutExpo", function(){
+            //     // $WO__main.css('padding-top', $WO__navbarHeight);
+            //     // Add hash (#) to URL when done scrolling (default click behavior)
+            //     // window.location.hash = hash;
+            // });
+            
+            
+            e.preventDefault();
+        }
+    });
 
     // Features
     var $feature = $('.feature'),
@@ -203,76 +183,4 @@
         if(e.keyCode==27){$WO__overlayClose.trigger('click');}
     });*/
     // end Overlay
-
-    //Video Resize
-    // function $TG__videosResize(){
-    //     var $allVideos = $('iframe[src^="//player.vimeo.com"], iframe[src^="https://www.youtube.com"], object, embed');
-
-    //     $allVideos.each(function(){
-    //         $(this)
-    //         // jQuery .data does not work on object/embed elements
-    //         .attr('data-aspectRatio', this.height / this.width)
-    //         .removeAttr('height')
-    //         .removeAttr('width');
-    //     });
-
-    //     $(window).resize(function(){//console.log('resizing');
-    //         var newWidth = $WO__overlayMedia.width();
-
-    //         $allVideos.each(function(){
-    //             var $el = $(this);
-
-    //             $el
-    //                 .width(newWidth)
-    //                 .height(newWidth * $el.attr('data-aspectRatio'));
-    //         });
-    //     }).resize();
-    // }
-
-
-
-
-
-
-    // $('.rutg-accordion').collapse({
-    //     toggle: false
-    // });
-
-    // $(".faq-sidebar-navigation ul li a").click(function(e) {
-    //     e.preventDefault();
-    //     var activeFaq = $(".faq-sidebar-navigation ul li.active a");
-    //     var id = activeFaq.attr('href');
-    //     var dest = $(this).attr('href');
-    //     activeFaq.parent().removeClass('active');
-    //     $(this).parent().addClass('active');
-    //     $(id).removeClass('active');
-    //     $(dest).addClass('active');
-    //     return false;
-    // });
-
-    // Magnific popup calls
-    // $('.head-play-button').magnificPopup({
-    //     tLoading: 'Loading image #%curr%...',
-    //     mainClass: 'mfp-img-mobile',
-    //     disableOn: 0,
-    //     type: 'iframe',
-    //     mainClass: 'mfp-fade',
-    //     removalDelay: 160,
-    //     preloader: true,
-    //     fixedContentPos: false,
-    //     iframe: {
-    //         patterns: {
-    //             youtube: {
-    //                 index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-
-    //                 id: 'v=', // String that splits URL in a two parts, second part should be %id%
-    //                 // Or null - full URL will be returned
-    //                 // Or a function that should return %id%, for example:
-    //                 // id: function(url) { return 'parsed id'; }
-    //                 src: 'https://www.youtube.com/embed/%id%?rel=0&autoplay=1' // URL that will be set as a source for iframe.
-    //             }
-    //         }
-    //     }
-    // });
-
 })(jQuery); // End of use strict
